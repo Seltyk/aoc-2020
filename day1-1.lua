f612f57f68cd7d1e307e9be64fb65d6db0efaa91
wundrweapon <whhacker.dcx@gmail.com>
2020-12-02 15:57:39 -0500

    Day 1: optimized Time complexities are now O(n) and O(n²) respectively. This relies on all values in the input list being unique to exploit Θ(1) set properties

--- day1-1.lua
+++ day1-1.lua
@@ -1,21 +1,24 @@
 #!/bin/lua5.4
 local f = assert(io.open("day1", 'r'))
-local ns = setmetatable({}, {__index = table})
-local l = f:read('l')
-while l and l ~= "" do
-	ns:insert(tonumber(l))
-	l = f:read('l')
+local ns = {}
+for l in f:lines() do
+	ns[tonumber(l)] = true
 end
 
 f:close(); f = nil
 
-for i = 1, #ns - 1 do
-	for j = i + 1, #ns do
-		if ns[i] + ns[j] == 2020 then
-			print(ns[i] * ns[j])
-			os.exit(0)
+for k, _ in pairs(ns) do
+	if arg[1] == '3' then
+		for j, _ in pairs(ns) do
+			if k ~= j and ns[2020 - k - j] then
+				print(k * j * (2020 - k - j))
+				os.exit()
+			end
+		end
+	elseif arg[1] == '2' then
+		if ns[2020 - k] then
+			print(k * (2020 - k))
+			os.exit()
 		end
 	end
 end
-
-os.exit(1)
