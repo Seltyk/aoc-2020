ee0d6b40861b570bf761ae38a17cd6eefe231da7
wundrweapon <whhacker.dcx@gmail.com>
2020-12-07 03:47:50 -0500

    Day 4: part 2 complete Lua's tables are a godsend

--- day4.lua
+++ day4.lua
@@ -1,7 +1,45 @@
 #!/bin/lua5.4
 
---List required passport entries
-local entries = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+--List required passport entries and their legality checks
+local entries = {
+	byr = function(v)
+		local n = tonumber(v)
+		return #v == 4 and n and 1920 <= n and n <= 2002
+	end,
+
+	iyr = function(v)
+		local n = tonumber(v)
+		return #v == 4 and n and 2010 <= n and n <= 2020
+	end,
+
+	eyr = function(v)
+		local n = tonumber(v)
+		return #v == 4 and n and 2020 <= n and n <= 2030
+	end,
+
+	hgt = function(v)
+		local n = tonumber(v:sub(1, -3))
+		if not n then return false end
+		if v:sub(-2) == "cm" then return 150 <= n and n <= 193
+		elseif v:sub(-2) == "in" then return 59 <= n and n <= 76
+		else return false end
+	end,
+
+	hcl = function(v)
+		if v:sub(1, 1) ~= '#' or #v:sub(2) ~= 6 then return false end
+		return tonumber(v:sub(2), 16)
+	end,
+
+	ecl = function(v)
+		local colors = {amb = true, blu = true, brn = true, gry = true, grn = true, hzl = true, oth = true}
+		return colors[v]
+	end,
+
+	pid = function(v)
+		if #v ~= 9 then return false end
+		return tonumber(v)
+	end
+}
 
 --Get input data
 local f = assert(io.open("day4", 'r'))
@@ -22,8 +60,15 @@ for line in input:gmatch(".-\n") do
 	end
 
 	--Ensure every required entry is there
-	for _, entry in ipairs(entries) do
+	for entry, check in pairs(entries) do
+
+		--Part 1 solution: is there or isn't there data
 		if not passport[entry] then goto invalid end
+
+		--Part 2 solution: is this data legal
+		if arg[1] == '2' then
+			if not check(passport[entry]) then goto invalid end
+		end
 	end
 
 	valid = valid + 1
