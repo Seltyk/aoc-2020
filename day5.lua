c42ac77a5fc6a05504bbf0f0df90b0afb0f0daf1
wundrweapon <whhacker.dcx@gmail.com>
2020-12-07 20:18:57 -0500

    Day 5: Lua I fucking LOVED this one. I saw the number 128, and the number 8, and an 8-digit binary sequence, and a 3-digit binary sequence, and I knew exactly where this was going. Admittedly I didn't immediately realize the whole thing was an 11-digit binary sequence, but my brain saw "multiply by 8" and defaulted to "left shift by 3" so the connection was made pretty quickly

---
+++ day5.lua
@@ -0,0 +1,27 @@
+#!/bin/lua5.4
+
+--Get input
+local f = assert(io.open("day5", 'r'))
+
+--Loop over lines and track either highest ID or existing seats
+local out = arg[1] == '2' and {} or 0
+for seat in f:lines() do
+	seat = tonumber(seat:gsub('F', '0'):gsub('B', '1'):gsub('L', '0'):gsub('R', '1'), 2)
+
+	if arg[1] == '2' then
+		out[seat] = true
+	else
+		if seat > out then out = seat end
+	end
+end
+
+if arg[1] == '2' then
+	for id = 1, 2046 do
+		if not out[id] and out[id - 1] and out[id + 1] then
+			out = id
+			break
+		end
+	end
+end
+
+print(out)
